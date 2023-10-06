53bc4b40ff7eb97d6e3cac93d33f7708728904f1
Cody Comstock <mr.codysherwood@hotmail.com>
2023-10-06 17:56:51 -0500

    create spring boot rest project and add jpa project dependencies

---
+++ BookLog/src/main/java/com/skilldistillery/booklog/BookLogApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.booklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLogApplication.class, args);
+	}
+
+}
